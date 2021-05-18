0a62ded5f226cdbbcbe21ac657f5eebe97af0230
Dmitryi Pletnev <dmpinform@gmail.com>
2021-05-18 23:41:13 +0300

    lesson7

--- lesson7-3.py
+++ lesson7-3.py
@@ -1,36 +1,36 @@
-class Kletka:
+class Cell:
     def __init__(self, cells: int):
         self.cells = cells
 
     def __add__(self, x):
-        if type(x) == Kletka:
-            return Kletka(self.cells + x.cells)
+        if type(x) == Cell:
+            return Cell(self.cells + x.cells)
         raise Exception("Error type")
 
     def __sub__(self, x):
-        if type(x) == Kletka and self.cells > x.cells:
-            return Kletka(self.cells - x.cells)
+        if type(x) == Cell and self.cells > x.cells:
+            return Cell(self.cells - x.cells)
         raise Exception("Error type or values")
 
     def __mul__(self, x):
-         if type(x) == Kletka:
-            return Kletka(self.cells * x.cells)
-         raise Exception("Error type")
+        if type(x) == Cell:
+            return Cell(self.cells * x.cells)
+        raise Exception("Error type")
 
     def __truediv__(self, x):
-         if type(x) == Kletka:
-            return Kletka(self.cells // x.cells)
-         raise Exception("Error type")
+        if type(x) == Cell:
+            return Cell(self.cells // x.cells)
+        raise Exception("Error type")
 
     def make_order(self, col):
-        kletkas = ['*' for _ in range(0, self.cells)]
-        return "\n".join(["".join(kletkas[i:i+col]) for i in range(0, len(kletkas), col)])
+        cells = ['*' for _ in range(0, self.cells)]
+        return "\n".join(["".join(cells[i:i + col]) for i in range(0, len(cells), col)])
 
 
-kletka1 = Kletka(30)
-kletka2 = Kletka(4)
+cell1 = Cell(30)
+cell2 = Cell(4)
 
-kletka3 = kletka1 + kletka2
+cell3 = cell1 + cell2
 
-print(kletka3.cells)
-print(kletka3.make_order(7))
+print(cell3.cells)
+print(cell3.make_order(10))
